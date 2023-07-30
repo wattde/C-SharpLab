9112d1410511513dc6e1ec16589e8ae711f03929
Wathsala De Silva <135297355+wattde@users.noreply.github.com>
2023-07-31 00:54:45 +0530

    Add files via upload

---
+++ C#Lab05/C#Lab05Q1/C#Lab05Q1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace C_Lab05Q1
+{
+    
+    
+        public static void ArrayOperations(int[] arr)
+        {
+            int min = arr[0];
+            int max = arr[0];
+            int sum = 0;
+
+            // Calculate min, max, and sum
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                    min = arr[i];
+
+                if (arr[i] > max)
+                    max = arr[i];
+
+                sum += arr[i];
+            }
+
+            double average = (double)sum / arr.Length;
+
+            // Display results
+            Console.WriteLine("Minimum value: " + min);
+            Console.WriteLine("Maximum value: " + max);
+            Console.WriteLine("Average value: " + average);
+
+            // Reverse the array
+            Array.Reverse(arr);
+            Console.WriteLine("Reverse order of values:");
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
